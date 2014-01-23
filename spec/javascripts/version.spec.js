829962009262f92b8326d587414adee22d1c48bd
Simon Oulevay <simon.oulevay@lotaris.com>
2014-01-23 11:49:48 +0100

    Update boot javascript when version changes so that asset pipeline recompiles the file.

--- spec/javascripts/version.spec.js
+++ spec/javascripts/version.spec.js
@@ -14,7 +14,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with ROX Center.  If not, see <http://www.gnu.org/licenses/>.
-
 describe("Version", function() {
 
   it("should be correct", function() {
