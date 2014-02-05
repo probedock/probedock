b41770634750836dc401e2bc9054b5b7406b4b8d
Simon Oulevay <simon.oulevay@lotaris.com>
2014-02-05 15:55:20 +0100

    Updated sample ldap configuration (don't use anonymous binding by default).

--- config/samples/initializers/ldap.rb
+++ config/samples/initializers/ldap.rb
@@ -7,7 +7,8 @@ Devise.setup do |config|
     config.ldap_config = "#{Rails.root}/config/ldap.yml"
     config.ldap_check_group_membership = false
     config.ldap_check_attributes = false
-    config.ldap_use_admin_to_bind = false
     config.ldap_ad_group_check = false
+    # set to false if you want to use anonymous binding
+    config.ldap_use_admin_to_bind = true
   end
 end
