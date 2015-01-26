8d87477a39b6b8ddea89b6c801b082f883db0586
Simon Oulevay <simon.oulevay@gmail.com>
2015-01-26 14:49:34 +0100

    Updated validator.

--- app/validations/project_validations.rb
+++ app/validations/project_validations.rb
@@ -1,4 +1,6 @@
 class ProjectValidations < Errapi::SingleValidator
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :description, length: { maximum: 1000 }
+  configure do
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :description, length: { maximum: 1000 }
+  end
 end
