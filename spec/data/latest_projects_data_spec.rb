7d28642be419784151e2a5e74ec83f3e4288f9ea
Simon Oulevay <simon.oulevay@gmail.com>
2015-02-10 15:51:02 +0100

    Renamed in license text.

--- spec/data/latest_projects_data_spec.rb
+++ spec/data/latest_projects_data_spec.rb
@@ -1,19 +1,19 @@
 # Copyright (c) 2012-2014 Lotaris SA
 #
-# This file is part of ROX Center.
+# This file is part of Probe Dock.
 #
-# ROX Center is free software: you can redistribute it and/or modify
+# Probe Dock is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ROX Center is distributed in the hope that it will be useful,
+# Probe Dock is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ROX Center.  If not, see <http://www.gnu.org/licenses/>.
+# along with Probe Dock.  If not, see <http://www.gnu.org/licenses/>.
 require 'spec_helper'
 
 describe LatestProjectsData do
