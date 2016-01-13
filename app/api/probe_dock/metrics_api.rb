a3283ead1b7eb0f08d7fd32e7cd1e2c334df16fa
Simon Oulevay <simon.oulevay@gmail.com>
2016-01-13 14:54:15 +0100

    Optimized team metrics.

--- app/api/probe_dock/metrics_api.rb
+++ app/api/probe_dock/metrics_api.rb
@@ -28,6 +28,8 @@ module ProbeDock
             Organization.active.where(api_id: params[:organizationId].to_s).first!
           elsif params[:organizationName].present?
             Organization.active.where(normalized_name: params[:organizationName].to_s.downcase).first!
+          elsif params[:projectId].present?
+            Organization.active.joins(:projects).where('projects.api_id = ?', params[:projectId].to_s).first!
           end
         end
       end
@@ -69,40 +71,72 @@ module ProbeDock
         result
       end
 
+      # GET /api/metrics/team?organizationId&projectId&withUser
+      #
+      # Returns the list of contributors for an organization or project.
+      # Ordered by descending number of tests and ascending user name.
+      #
+      #     [
+      #       {
+      #         "userId": "abcde",
+      #         "testsCount": 123,
+      #         "categories": [ "JUnit", "Karma" ]
+      #       },
+      #       {
+      #         "userId": "bcdef",
+      #         "testsCount": 45,
+      #         "categories": [ "JUnit", "Karma" ]
+      #       }
+      #     ]
       get :team do
         authorize! :organization, :data
 
+        # load optional project filter
         project = nil
         project = Project.where(organization_id: current_organization.id, api_id: params[:projectId]).first! if params[:projectId]
 
-        category_rel = if project
-          Category.joins(test_descriptions: [ :contributors, :project_version ]).where 'project_versions.project_id = ?', project.id
-        else
-          Category.joins(test_descriptions: :contributors)
+        # query all categories linked to the organization, and all contributors that have written tests for those categories
+        category_joins = { test_descriptions: [ :contributors, { test: :project } ] }
+        category_select = 'categories.*, array_agg(distinct test_contributors.user_id) AS contributor_ids'
+        category_where = [ 'projects.organization_id = ?', current_organization.id ]
+
+        # if a project ID is given, limit the query to the project in question
+        if project
+          category_joins[:test_descriptions][1] = :test # no need to join with the projects table any more
+          category_where = [ 'project_tests.project_id = ?', project.id ]
         end
 
-        with_user = true_flag? :withUser
+        # load the categories
+        categories_with_contributors = Category.select(category_select).joins(category_joins).where(*category_where).group('categories.id').to_a
 
+        # query all users that have contributed within the organization, and the number of tests they have written
         user_joins = { test_contributors: { test_description: { test: :project } } }
-        user_select_clause = 'users.*, count(distinct project_tests.id) AS tests_count'
+        user_select = 'users.*, count(distinct project_tests.id) AS tests_count'
         user_order = 'tests_count DESC, users.name ASC'
 
         user_conditions = [ 'projects.organization_id = ?' ]
         user_values = [ current_organization.id ]
 
+        # if a project ID is given, limit the query to the project in question
         if project
           user_conditions << 'projects.id = ?'
           user_values << project.id
         end
 
-        user_where_clause = user_values.unshift user_conditions.join(' AND ')
+        user_where = user_values.unshift user_conditions.join(' AND ')
 
-        User.select(user_select_clause).joins(user_joins).where(user_where_clause).group('users.id').order(user_order).to_a.collect do |user|
+        # load the users
+        contributors = User.select(user_select).joins(user_joins).where(user_where).group('users.id').order(user_order).to_a
+
+        # send the response
+        with_user = true_flag? :withUser
+
+        contributors.collect do |user|
 
           contributor = {
             userId: user.api_id,
             testsCount: user.tests_count,
-            categories: category_rel.where('test_contributors.user_id = ?', user.id).distinct.pluck(:name)
+            categories: categories_with_contributors.select{ |cat| cat.contributor_ids.include? user.id }.collect(&:name).sort
           }
 
           contributor[:user] = policy_serializer(user).serialize if with_user
