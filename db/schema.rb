a6b791ae9532d46f78b1044917604a721cee1164
Laurent Prevost <laurent.prevost@probedock.io>
2016-01-14 13:24:14 +0100

    Applied migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114085241) do
+ActiveRecord::Schema.define(version: 20160114090321) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
