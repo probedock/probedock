de453901228f98a4188be0c539842f554f3aee10
Laurent Prevost <laurent.prevost@probedock.io>
2016-01-14 17:24:30 +0100

    Working in progress for the test details page (first experimental draft)

---
+++ app/assets/javascripts/modules/test.js
@@ -0,0 +1,18 @@
+angular.module('probedock.test', [ 'probedock.api', 'probedock.forms', 'probedock.utils' ])
+
+  .controller('TestCtrl', function (api, forms, orgs, projects, $scope, $state, $stateParams) {
+    orgs.forwardData($scope);
+
+    api({
+      url: '/tests/' + $stateParams.testId,
+      params: {
+        organizationId: $stateParams.orgId,
+        withProject: 1
+      }
+    })
+    .then(function (response) {
+      $scope.test = response.data;
+    });
+  })
+
+;
