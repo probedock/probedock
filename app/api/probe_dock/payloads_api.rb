35acb238c3285fe1b720f9a2a5b6b6c3e52758dd
Simon Oulevay <simon.oulevay@gmail.com>
2016-01-22 15:35:09 +0100

    Payloads api comments [ci skip].

--- app/api/probe_dock/payloads_api.rb
+++ app/api/probe_dock/payloads_api.rb
@@ -20,8 +20,11 @@ module ProbeDock
     format :json
     default_format :json
 
+    # accept Probe Dock JSON payloads
     content_type :payload_v1, 'application/vnd.probedock.payload.v1+json'
     content_type :json, 'application/json'
+
+    # accept xUnit XML payloads
     content_type :xml, 'application/xml'
 
     # disable automatic parsing by Grape for this resource
@@ -45,11 +48,15 @@ module ProbeDock
           raw_payload_type = nil
           body = env['api.request.input']
 
+          # parse and validate test payload with the
+          # correct parser depending on the content type
           case request.content_type
+          # parse Probe Dock JSON payload
           when Mime::Type.lookup_by_extension(:json), Mime::Type.lookup_by_extension(:payload_v1)
             raw_payload_type = :json
             raw_payload = MultiJson.load body
             TestPayloadValidations.validate raw_payload, validation_context, location_type: :json, raise_error: true
+          # parse xUnit XML payload
           when Mime::Type.lookup_by_extension(:xml)
             raw_payload_type = :xml
             raw_payload = TestPayloadXunitParser.new(body, headers).parse
@@ -69,15 +76,19 @@ module ProbeDock
 
           project_version = raw_payload['version']
 
+          # save the payload to the database
           payload = TestPayload.new runner: current_user, received_at: received_at, ended_at: ended_at
           payload.contents = raw_payload
           payload.contents_bytesize = body.bytesize
+
+          # save the original payload if not JSON (e.g. xUnit payload)
           payload.raw_contents = body if raw_payload_type != :json
 
           unless payload.save
             return record_errors payload
           end
 
+          # add the payload to the asynchronous processing queue
           Resque.enqueue ProcessNextTestPayloadJob
 
           status 202
