8272f590585a361f843d197f2b4aa291cc15c187
Simon Oulevay <simon.oulevay@gmail.com>
2016-01-22 11:39:07 +0100

    xUnit parser improvements and comments.

--- app/api/probe_dock/payloads_api.rb
+++ app/api/probe_dock/payloads_api.rb
@@ -24,6 +24,7 @@ module ProbeDock
     content_type :json, 'application/json'
     content_type :xml, 'application/xml'
 
+    # disable automatic parsing by Grape for this resource
     parser :payload_v1, nil
     parser :json, nil
     parser :xml, nil
@@ -41,37 +42,32 @@ module ProbeDock
           received_at = Time.now.utc
 
           raw_payload = nil
-          raw_payload_type = :json
+          raw_payload_type = nil
           body = env['api.request.input']
 
           case request.content_type
           when Mime::Type.lookup_by_extension(:json), Mime::Type.lookup_by_extension(:payload_v1)
+            raw_payload_type = :json
             raw_payload = MultiJson.load body
             TestPayloadValidations.validate raw_payload, validation_context, location_type: :json, raise_error: true
           when Mime::Type.lookup_by_extension(:xml)
             raw_payload_type = :xml
-            raw_payload = TestPayloadXunitParser.new(body, headers['Probe-Dock-Duration']).parse
+            raw_payload = TestPayloadXunitParser.new(body, headers).parse
           else
             status 415
             return nil
           end
 
-          if raw_payload['reports'].blank? && uid = headers['Probe-Dock-Test-Report-Uid']
-            raw_payload['reports'] = [ { 'uid' => uid } ]
-          end
-
           # FIXME: do not accept payloads in the future
           ended_at = received_at
-          ended_at = Time.parse raw_payload['endedAt'] if raw_payload_type == :json && raw_payload.key?('endedAt')
+          ended_at = Time.parse raw_payload['endedAt'] if raw_payload.key?('endedAt')
 
           # FIXME: foreign key validation
-          project_api_id = raw_payload.fetch('projectId', headers['Probe-Dock-Project-Id']).to_s
-          project = Project.where(api_id: project_api_id).first
+          project_api_id = raw_payload['projectId']
+          project = Project.where(api_id: project_api_id.to_s).first
           Pundit.authorize current_user, project, :publish?
-          raw_payload['projectId'] ||= project_api_id
 
-          project_version = raw_payload.fetch('version', headers['Probe-Dock-Project-Version']).to_s
-          raw_payload['version'] ||= project_version
+          project_version = raw_payload['version']
 
           payload = TestPayload.new runner: current_user, received_at: received_at, ended_at: ended_at
           payload.contents = raw_payload
