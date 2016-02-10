5e11ecde265ec1ca0ac68e1b0085c525f4bb300a
Laurent Prevost <laurent.prevost@probedock.io>
2016-02-10 18:33:10 +0100

    Added the support of named record scoped by record types

--- spec/support/named_records_spec_helper.rb
+++ spec/support/named_records_spec_helper.rb
@@ -16,15 +16,55 @@
 # You should have received a copy of the GNU General Public License
 # along with ProbeDock.  If not, see <http://www.gnu.org/licenses/>.
 module NamedRecordsSpecHelper
-  def add_named_record name, record
+  def add_named_record(name, record)
     @named_records ||= {}
-    raise "A named record already exists for name #{name.inspect}" if @named_records[name]
-    @named_records[name] = record
+    key = NamedRecordKey.new(name, record)
+    raise "A named record already exists for #{key}" if @named_records[key]
+    @named_records[key] = record
   end
 
-  def named_record name
-    record = @named_records.try :[], name
-    raise "Unknown named record #{name.inspect}" unless record
+  def named_record(name)
+    records = @named_records.select do |key, value|
+      key.name == name
+    end
+
+    raise "Unknown named record #{name.inspect}" if records.length == 0
+    raise "More than one named record #{name.inspect}" if records.length > 1
+
+    records.values.first
+  end
+
+  def named_record_by_type(name, type)
+    key = NamedRecordKey.new(name, type)
+    record = @named_records.try(:[], key)
+    raise "Unknown named record #{key}" unless record
     record
+  end
+
+  class NamedRecordKey
+    attr_reader :name, :type
+
+    def initialize(name, type)
+      @name = name
+      if type.is_a?(String)
+        @type = type
+      elsif type.is_a?(Class)
+        @type = type.name
+      else
+        @type = type.class.name
+      end
+    end
+
+    def eql?(named_record_key)
+      name == named_record_key.name && type == named_record_key.type
+    end
+
+    def hash
+      name.hash + type.hash
+    end
+
+    def to_s
+      "name: #{name}, type: #{type}"
+    end
   end
 end
