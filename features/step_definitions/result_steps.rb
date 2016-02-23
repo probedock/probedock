f0e9ad3740a96ef3fafd0946c65001720241d811
Laurent Prevost <laurent.prevost@probedock.io>
2016-02-23 18:13:32 +0100

    Added /reports API tests for filtering

--- features/step_definitions/result_steps.rb
+++ features/step_definitions/result_steps.rb
@@ -1,7 +1,8 @@
-Given /^result (.*) for test "(.+)"(?: is(?: (new) and)?(?: (passing|failing) and)?(?: (active|inactive) and)?)? was run(?: (\d*) ((?:day|week)s?) ago)? by (.+?)(?: and took (\d+) second(?:s) to run)? for payload (.+?)(?: at index (\d+))? with version (.+)$/ do |name,test_name,new_test,passing,active,interval_count,interval,runner_name,execution_time,payload_name,payload_index,project_version|
-  runner = named_record runner_name
-  project_version = named_record project_version
-  payload = named_record payload_name
+Given /^result (.*) for test "(.+)"(?: is(?: (new) and)?(?: (passing|failing) and)?(?: (active|inactive) and)?)?(?: has category (.+?) and)? was run(?: (\d*) ((?:day|week)s?) ago)? by (.+?)(?: and took (\d+) second(?:s) to run)? for payload (.+?)(?: at index (\d+))? with version (.+)$/ do |name,test_name,new_test,passing,active,category_name,interval_count,interval,runner_name,execution_time,payload_name,payload_index,project_version|
+  runner = named_record(runner_name)
+  project_version = named_record(project_version)
+  payload = named_record(payload_name)
+  test = named_record(test_name)
 
   date = if interval_count
     interval_count.to_i.send(interval).ago
@@ -14,10 +15,9 @@ Given /^result (.*) for test "(.+)"(?: is(?: (new) and)?(?: (passing|failing) an
     runner: runner,
     project_version: project_version,
     test_payload: payload,
-    run_at: date
+    run_at: date,
+    test: test
   }
-
-  options[:test] = named_record test_name
 
   if passing
     options[:passed] = passing == 'passing'
@@ -33,11 +33,43 @@ Given /^result (.*) for test "(.+)"(?: is(?: (new) and)?(?: (passing|failing) an
     options[:duration] = execution_time.to_i
   end
 
+  if category_name
+    category = named_record(category_name) if named_record_exists(category_name)
+
+    unless category
+      category = create(:category, name: category_name)
+      add_named_record(category_name, category)
+    end
+
+    options[:category] = category
+  end
+
   options[:payload_index] = if payload_index
     payload_index
   else
     TestResult.select('COALESCE(MAX(payload_index) + 1, 0) as next_payload_index').where('test_payload_id = ?', payload.id).take.next_payload_index
   end
 
-  add_named_record name, create(:test_result, options)
+  test_result = add_named_record(name, create(:test_result, options))
+
+
+  test_description = TestDescription.where(test_id: test.id).where(project_version_id: project_version.id).first
+
+  if !test_description
+    description_options = {
+      test: test,
+      last_runner: runner,
+      project_version: project_version,
+      active: test_result.active,
+      passing: test_result.passed,
+      last_run_at: test_result.run_at,
+      last_duration: test_result.duration
+    }
+
+    if category
+      description_options[:category] = category
+    end
+
+    create(:test_description, description_options)
+  end
 end
