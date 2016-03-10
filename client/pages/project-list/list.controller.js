0c7000ba6cfac8b1c59d0d2aa917ea000af33c7e
Laurent Prevost <laurent.prevost@probedock.io>
2016-03-10 17:08:07 +0100

    Added project filter by display name on projects list

--- client/pages/project-list/list.controller.js
+++ client/pages/project-list/list.controller.js
@@ -1,14 +1,14 @@
 angular.module('probedock.projectListPage').controller('ProjectListPageCtrl', function(api, orgs, projectEditModal, $scope, $state, $stateParams) {
   orgs.forwardData($scope);
-
-  // FIXME: recursively fetch all projects
-  api({
-    url: '/projects',
-    params: {
-      organizationName: $stateParams.orgName,
-      pageSize: 25
+  //
+  //fetchProjects();
+  //
+  $scope.$watch('projectName', function(value, oldValue) {
+    if (value || value !== oldValue) {
+      //fetchProjects();
+      filterProjects();
     }
-  }).then(showProjects);
+  });
 
   $scope.$on('$stateChangeSuccess', function(event, toState, toStateParams) {
     if (toState.name.match(/^org.projects.list.(?:new|edit)$/)) {
@@ -26,10 +26,60 @@ angular.module('probedock.projectListPage').controller('ProjectListPageCtrl', fu
   });
 
   $scope.orderProject = function(project) {
-    return (project.displayName || project.name).toLowerCase();
+    return (project.project.displayName || project.project.name).toLowerCase();
   };
 
-  function showProjects(response) {
-    $scope.projects = response.data;
+  $scope.page = 0;
+  $scope.projects = [];
+
+  $scope.fetchProjects = function() {
+    if ($scope.disableScroll) {
+      return;
+    }
+
+    $scope.loading = true;
+    $scope.disableScroll = true;
+
+    $scope.page++;
+    var params = {
+      organizationName: $stateParams.orgName,
+      pageSize: 1,
+      page: $scope.page
+    };
+
+    api({
+      url: '/projects',
+      params: params
+    }).then(function(response) {
+      $scope.projects = $scope.projects.concat(prepareProjects(response.data));
+      $scope.total = response.pagination().total;
+      $scope.disableScroll = $scope.page >= response.pagination().numberOfPages;
+      $scope.loading = false;
+    });
+  };
+
+  function prepareProjects(projects) {
+    return _.map(projects, function(project) {
+      return {
+        show:  isProjectShowable(project),
+        project: project
+      }
+    });
+  }
+
+  function filterProjects() {
+    _.each($scope.projects, function(project) {
+      project.show = isProjectShowable(project.project);
+    });
+  }
+
+  function isProjectShowable(project) {
+    if ($scope.projectName) {
+      var displayName = project.displayName ? project.displayName.toLowerCase() : '';
+      var search = $scope.projectName.toLowerCase();
+      return displayName.indexOf(search) > -1;
+    } else {
+      return true;
+    }
   }
 });
