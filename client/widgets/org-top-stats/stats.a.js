c44c0b4444fa4886b8f06cfdd4217a40c8e8f2d2
Laurent Prevost <laurent.prevost@probedock.io>
2016-03-17 23:51:33 +0100

    Added the widget to show the top five of orgs db stats

---
+++ client/widgets/org-top-stats/stats.a.js
@@ -0,0 +1,1 @@
+angular.module('probedock.orgTopStatsWidget', [ 'probedock.api' ]);
---
+++ client/widgets/org-top-stats/stats.directive.js
@@ -0,0 +1,45 @@
+angular.module('probedock.orgTopStatsWidget').directive('orgTopStatsWidget', function() {
+  return {
+    restrict: 'E',
+    controller: 'OrgTopStatsWidgetCtrl',
+    templateUrl: '/templates/widgets/org-top-stats/stats.template.html',
+    scope: {
+      top: '=?'
+    }
+  };
+}).controller('OrgTopStatsWidgetCtrl', function(api, $scope, orgs) {
+  _.defaults($scope, {
+    params: {
+      top: $scope.top ? $scope.top : 5
+    },
+    loading: true
+  });
+
+  fetchStats();
+
+  function fetchStats() {
+    $scope.loading = true;
+
+    return api({
+      url: '/platformManagement/orgStats',
+      params: $scope.params
+    }).then(function(response) {
+      if (response.data) {
+        $scope.stats = response.data.organizations;
+        $scope.total = _.omit(response.data, 'organizations');
+
+        $scope.top = _.reduce(response.data.organizations, function(memo, orgStats) {
+          memo.payloadsCount += orgStats.payloadsCount;
+          memo.projectsCount += orgStats.projectsCount;
+          memo.testsCount += orgStats.testsCount;
+          memo.resultsCount += orgStats.resultsCount;
+          return memo;
+        }, { payloadsCount: 0, projectsCount: 0, testsCount:0, resultsCount: 0 });
+
+        $scope.loading = false;
+      }
+
+      $scope.started = true;
+    });
+  }
+});
