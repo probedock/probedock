b24082d936f602826dd673ec75114e5ac99789c0
Laurent Prevost <laurent.prevost@probedock.io>
2016-03-17 16:32:52 +0100

    Added a filter to GET organizations

--- app/api/probe_dock/organizations_api.rb
+++ app/api/probe_dock/organizations_api.rb
@@ -22,7 +22,7 @@ module ProbeDock
 
       helpers do
         def parse_organization
-          parse_object :name, :displayName, :public
+          parse_object(:name, :displayName, :public)
         end
 
         def serialization_options orgs
@@ -44,50 +44,60 @@ module ProbeDock
 
       post do
         authenticate!
-        authorize! Organization, :create
+        authorize!(Organization, :create)
 
         data = parse_organization
         data[:active] = true
-        data[:public_access] = data.delete :public if data.key? :public
+        data[:public_access] = data.delete(:public) if data.key?(:public)
 
-        organization = Organization.new data
-        OrganizationValidations.validate organization, validation_context, location_type: :json, raise_error: true
+        organization = Organization.new(data)
+        OrganizationValidations.validate(organization, validation_context, location_type: :json, raise_error: true)
 
-        create_record organization
+        create_record(organization)
       end
 
       get do
         authenticate
-        authorize! Organization, :index
+        authorize!(Organization, :index)
 
-        rel = policy_scope(Organization).order 'name ASC'
+        rel = policy_scope(Organization).order('name ASC')
 
         rel = paginated rel do |rel|
           if params[:search].present?
             term = "%#{params[:search].downcase}%"
-            rel = rel.where 'LOWER(api_id) LIKE ? OR LOWER(name) LIKE ?', term, term
+            rel = rel.where('LOWER(api_id) LIKE ? OR LOWER(name) LIKE ?', term, term)
           end
 
           if params[:name].present?
-            rel = rel.where normalized_name: params[:name].to_s.downcase
+            rel = rel.where(normalized_name: params[:name].to_s.downcase)
           end
 
-          if params.key? :public
-            rel = rel.where public_access: true_flag?(:public)
+          if params.key?(:public)
+            rel = rel.where(public_access: true_flag?(:public))
           end
 
           if params.key?(:accessible) && !current_user.try(:admin?)
             if current_user
-              rel = rel.joins('LEFT OUTER JOIN memberships ON organizations.id = memberships.organization_id').where('organizations.public_access = ? OR (memberships.id IS NOT NULL AND memberships.user_id = ?)', true, current_user.id).distinct
+              rel = rel.joins('LEFT OUTER JOIN memberships ON organizations.id = memberships.organization_id')
+                .where('organizations.public_access = ? OR (memberships.id IS NOT NULL AND memberships.user_id = ?)', true, current_user.id).distinct
             else
-              rel = rel.where 'organizations.public_access = ?', true
+              rel = rel.where('organizations.public_access = ?', true)
             end
+          end
+
+          # Retrieve the organization for which the user is an admin
+          if params.key?(:administered)
+            # Force a user to be connected
+            authenticate!
+
+            rel = rel.joins('LEFT OUTER JOIN memberships ON organizations.id = memberships.organization_id')
+              .where('(memberships.roles_mask & ? != 0 AND memberships.user_id = ?)', Membership.mask_for(:admin), current_user.id).distinct
           end
 
           rel
         end
 
-        serialize load_resources(rel)
+        serialize(load_resources(rel))
       end
 
       namespace '/:id' do
@@ -99,18 +109,18 @@ module ProbeDock
 
         get do
           authenticate
-          authorize! record, :show
+          authorize!(record, :show)
           serialize record
         end
 
         patch do
           authenticate!
-          authorize! record, :update
+          authorize!(record, :update)
 
           updates = parse_organization
-          updates[:public_access] = updates.delete :public if updates.key? :public
+          updates[:public_access] = updates.delete(:public) if updates.key?(:public)
 
-          update_record record, updates
+          update_record(record, updates)
         end
       end
     end
