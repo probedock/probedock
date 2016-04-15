022da8f6859175f891772ec1c295c5f340972e68
Laurent Prevost <laurent.prevost@probedock.io>
2016-04-15 14:32:35 +0200

    Fixed PR comments

--- db/migrate/20160413074642_fix_long_names_in_xml_reports.rb
+++ db/migrate/20160413074642_fix_long_names_in_xml_reports.rb
@@ -2,22 +2,16 @@ class FixLongNamesInXmlReports < ActiveRecord::Migration
   def up
     test_results_rel = TestResult
       .joins(:test_payload)
-      .where('test_payloads.raw_contents IS NOT NULL')
+      .where("test_payloads.raw_contents IS NOT NULL AND test_results.name NOT LIKE '% %'")
     total = test_results_rel.count
     i = 0
 
     # Rename the test results for XML payloads
     test_results_rel.select(:id, :name).find_in_batches batch_size: 500 do |results|
       say_with_time "rename for results #{i + 1}-#{i + results.length} (out of #{total})" do
-        results_to_update = {}
-
         results.each do |result|
-          results_to_update[result.id] = {
-            name: result.name.underscore.humanize
-          }
+          TestResult.where(id: result.id).update_all(name: result.name.underscore.humanize)
         end
-
-        TestResult.update(results_to_update.keys, results_to_update.values)
 
         results.length
       end
@@ -27,24 +21,18 @@ class FixLongNamesInXmlReports < ActiveRecord::Migration
 
     project_tests_rel = ProjectTest
       .joins(results: :test_payload)
-      .where('test_payloads.raw_contents IS NOT NULL')
+      .where("test_payloads.raw_contents IS NOT NULL AND project_tests.name NOT LIKE '% %'")
       .reorder('')
-    total = project_tests_rel.select('COUNT(DISTINCT project_tests.id) AS project_tests_count').first.project_tests_count
+    total = project_tests_rel.count('DISTINCT project_tests.id')
     i = 0
 
     # Rename the tests for XML payloads
     project_tests_rel.select('DISTINCT project_tests.id, project_tests.name').find_in_batches batch_size: 500 do |tests|
 
       say_with_time "rename for tests #{i + 1}-#{i + tests.length} (out of #{total})" do
-        tests_to_update = {}
-
         tests.each do |test|
-          tests_to_update[test.id] = {
-            name: test.name.underscore.humanize
-          }
+          ProjectTest.where(id: test.id).update_all(name: test.name.underscore.humanize)
         end
-
-        ProjectTest.update(tests_to_update.keys, tests_to_update.values)
 
         tests.length
       end
@@ -54,24 +42,18 @@ class FixLongNamesInXmlReports < ActiveRecord::Migration
 
     test_descriptions_rel = TestDescription
       .joins(test: { results: :test_payload })
-      .where('test_payloads.raw_contents IS NOT NULL')
+      .where("test_payloads.raw_contents IS NOT NULL AND test_descriptions.name NOT LIKE '% %'")
       .reorder('')
-    total = test_descriptions_rel.select('COUNT(DISTINCT test_descriptions.id) AS test_descriptions_count').first.test_descriptions_count
+    total = test_descriptions_rel.count('DISTINCT test_descriptions.id')
     i = 0
 
     # Rename the test descriptions for XML payloads
     test_descriptions_rel.select('DISTINCT test_descriptions.id', 'test_descriptions.name').find_in_batches batch_size: 500 do |descriptions|
 
       say_with_time "rename for descriptions #{i + 1}-#{i + descriptions.length} (out of #{total})" do
-        descriptions_to_update = {}
-
         descriptions.each do |description|
-          descriptions_to_update[description.id] = {
-            name: description.name.underscore.humanize
-          }
+          TestDescription.where(id: description.id).update_all(name: description.name.underscore.humanize)
         end
-
-        TestDescription.update(descriptions_to_update.keys, descriptions_to_update.values)
 
         descriptions.length
       end
