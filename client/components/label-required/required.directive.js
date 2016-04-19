1054d7f8a8c4182a360664b7a44000ee1ff2fdac
Laurent Prevost <laurent.prevost@probedock.io>
2016-04-19 15:32:03 +0200

    Used transclusion

--- client/components/label-required/required.directive.js
+++ client/components/label-required/required.directive.js
@@ -1,10 +1,10 @@
 angular.module('probedock.labelRequired').directive('labelRequired', function() {
   return {
     restrict: 'E',
+    transclude: true,
     templateUrl: '/templates/components/label-required/required.template.html',
     scope: {
-      label: '@',
-      for: '@'
+      label: '@'
     }
   };
 });
