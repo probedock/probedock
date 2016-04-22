ea98082a2037c4a523c5fa0d47728c2d4a0f234c
Simon Oulevay <simon.oulevay@gmail.com>
2016-04-22 09:20:01 +0200

    Used replace and ng-attr-for in label-required directive.

--- client/components/label-required/required.directive.js
+++ client/components/label-required/required.directive.js
@@ -2,6 +2,7 @@ angular.module('probedock.labelRequired').directive('labelRequired', function() 
   return {
     restrict: 'E',
     transclude: true,
+    replace: true,
     templateUrl: '/templates/components/label-required/required.template.html',
     scope: {
       label: '@'
