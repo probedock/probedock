c91e2f44adc4ae94ef4c727e72f64507268cdfe0
Berthouzoz Michaël <hello@michael-berthouzoz.me>
2016-07-20 11:43:03 +0200

    change width min

--- client/widgets/new-tests/newtests.controller.js
+++ client/widgets/new-tests/newtests.controller.js
@@ -20,7 +20,7 @@ angular.module('probedock.newTestsWidget').controller('NewTestsContentCtrl', ['$
     MONTH_LABEL_PADDING = 6,
     PADDING_TOP = 20,
     PADDING_LEFT = 20,
-    WIDTH_MIN = 560,
+    WIDTH_MIN = 700,
     svg;
 
   $(window).resize(function () {
