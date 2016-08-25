b9a100b7f2761f641ea1f349100ca82bef50ff04
Laurent Prevost <laurent.prevost@probedock.io>
2016-08-25 09:46:39 +0200

    Added two more params (missing ones from previous commit)

--- client/routes.js
+++ client/routes.js
@@ -32,7 +32,7 @@ angular.module('probedock.routes', [ 'probedock.states', 'ui.router' ])
       })
 
       .state('permalinks', {
-        url: '/go/:target?uid&organizationId',
+        url: '/go/:target?id&organizationId&payloadId&uid',
         controller: 'PermalinksPageCtrl',
         templateUrl: '/templates/pages/permalinks/permalinks.template.html'
       })
